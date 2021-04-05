fb7085bae91207761abf3287aa0db9bcb409c8d0
eumc <livelch012@naver.com>
2021-04-05 11:22:27 +0900

    add "Member DTO"

---
+++ src/com/web/DTO/Member.java
@@ -0,0 +1,62 @@
+package com.web.DTO;
+
+public class Member {
+	
+	private String no;
+	private String email;
+	private String phonNum;
+	private String residentNum;//ÁÖ¹Î¹øÈ£
+	private String addr;
+	
+	private String pw;
+	private String chkpw;
+	
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhonNum() {
+		return phonNum;
+	}
+	public void setPhonNum(String phonNum) {
+		this.phonNum = phonNum;
+	}
+	public String getResidentNum() {
+		return residentNum;
+	}
+	public void setResidentNum(String residentNum) {
+		this.residentNum = residentNum;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getChkpw() {
+		return chkpw;
+	}
+	public void setChkpw(String chkpw) {
+		this.chkpw = chkpw;
+	}
+	
+	
+	
+	
+	
+}
