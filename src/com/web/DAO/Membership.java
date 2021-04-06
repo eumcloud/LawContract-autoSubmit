f3778043602ce9cf15fedef785109887d0493a61
sophiea0110 <sophiea0110@naver.com>
2021-04-06 20:11:26 +0900

    -

--- src/com/web/DAO/Membership.java
+++ src/com/web/DAO/Membership.java
@@ -12,12 +12,9 @@ public class Membership {
 
 
 	public Connection getConn() {
-//		String url="jdbc:oracle:thin:@192.168.0.21:1521:xe";
-//		String usr = "c##acon";
-//		String pass= "1234";
-		String url="jdbc:oracle:thin:@localhost:1522:xe";
-		String usr="c##jin";
-		String pass="jin1234";
+		String url="jdbc:oracle:thin:@192.168.0.21:1521:xe";
+		String usr = "c##acon";
+		String pass= "1234";
 		Connection conn = null;
 		
 		
